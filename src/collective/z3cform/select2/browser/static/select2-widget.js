5ce3cbb471c2eeec84bb2e5fb60e985115906805
Martin Peeters <martin.peeters@affinitic.be>
2016-12-27 10:18:14 +0100

    Add a helper function to initialize select2 single widget

--- src/collective/z3cform/select2/browser/static/select2-widget.js
+++ src/collective/z3cform/select2/browser/static/select2-widget.js
@@ -1,15 +1,22 @@
-jQuery(document).ready(function($) {
+var initializeSelect2SingleWidget = function(obj) {
 
   var format = function(state) {
       var option = $(state.element);
       return '<span class="' + option.data('css') + '">' + state.text + '</span>';
   };
 
-  $('.single-select2-widget').select2({
+  $(obj).select2({
       width: 'resolve',
       formatResult: format,
       formatSelection: format,
       escapeMarkup: function(m) { return m; },
+  });
+};
+
+jQuery(document).ready(function($) {
+
+  $('.single-select2-widget').each(function() {
+    initializeSelect2SingleWidget($(this));
   });
 
 });
